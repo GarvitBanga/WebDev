14aa9c883b2a1004713065ccf79d096ceeab83e2
Garvit Banga <bangaboy181@gmail.com>
2025-03-09 00:05:48 -0500

    todolist

--- ToDoListapp/script.js
+++ ToDoListapp/script.js
@@ -4,7 +4,26 @@ function addTodo(){
     let element=document.createElement("div");
     element.setAttribute("id","todo"+ctr);
     element.setAttribute("class","todoelement");
-    element.innerHTML="<h4>"+ctr+". "+"<h4>"+input.value+"</h4> <button onclick='deleteTodo("+ctr+")'>Delete</button> <button onclick='updateTodo("+ctr+")'>Update</button>";
+
+    const txtelement1=document.createElement("h4");
+    txtelement1.innerHTML=ctr+". ";
+    element.appendChild(txtelement1);
+
+    const txtelement2=document.createElement("h4");
+    txtelement2.innerHTML=input.value;
+    element.appendChild(txtelement2);
+
+    const btnelement=document.createElement("button");
+    btnelement.innerHTML="Delete";
+    btnelement.setAttribute("onclick","deleteTodo("+ctr+")");
+    element.appendChild(btnelement);
+
+    const btnelement2=document.createElement("button");
+    btnelement2.innerHTML="Update";
+    btnelement2.setAttribute("onclick","updateTodo("+ctr+")");
+    element.appendChild(btnelement2);
+
+    // element.innerHTML="<h4>"+ctr+". "+"<h4>"+input.value+"</h4> <button onclick='deleteTodo("+ctr+")'>Delete</button> <button onclick='updateTodo("+ctr+")'>Update</button>";
     ctr++;
     document.querySelector("#todoparent").appendChild(element);
 }
