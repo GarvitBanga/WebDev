3c48f2361d1009ceb85ef463877b2dbc022f254a
Garvit Banga <bangaboy181@gmail.com>
2025-03-10 17:39:33 -0400

    httpserver notes

--- prac.js
+++ prac.js
@@ -12,4 +12,3 @@ console.log(todos[0].title);
 
 
 
-// Ports are logical endpoints used by protocols to identify specific processes running on a computer or server. They help direct network traffic to the correct application or process on a system.
