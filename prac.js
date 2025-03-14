20fb4d24e82252da42461e745905dfe34ba7ae73
Garvit Banga <bangaboy181@gmail.com>
2025-03-14 19:33:16 -0400

    map and filter

--- prac.js
+++ prac.js
@@ -12,3 +12,40 @@ console.log(todos[0].title);
 
 
 
+
+
+// Map Filter 
+const input=[1,2,3,4,5,6,7,8,9,10];
+
+ const newArray=[];
+
+ for(let i=0;i<input.length;i++){
+    newArray.push(input[i]*2);
+ }
+
+ console.log(newArray);
+// You can also use map function
+ function transform(x){
+   return x*2;
+ }
+ const ans=input.map(transform);
+ console.log(ans);
+
+
+//  Filter
+
+const arr=[1,2,3,4,5,6,7,8,9,10];
+
+const newarray=[];
+
+for(let i=0;i<arr.length;i++){
+    if(arr[i]%2==0){
+        newarray.push(arr[i]);
+    }
+}
+
+console.log(newarray);
+
+const ans1=arr.filter((x)=>{return x%2==0;});
+
+console.log(ans1);
