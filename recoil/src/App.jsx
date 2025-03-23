de5a986914bff12f75e5784e9dca6c34971f666e
Garvit Banga <bangaboy181@gmail.com>
2025-03-23 02:22:17 -0400

    recoil selector memo

--- recoil/src/App.jsx
+++ recoil/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { RecoilRoot, atom, useSetRecoilState, useRecoilValue } from 'recoil';
-import { counterAtom } from './store/atoms/counter';
+import { counterAtom, evenSelector } from './store/atoms/counter';
 import { use } from 'react';
 
 function App() {
@@ -11,13 +11,65 @@ function App() {
   return (
   
       <div>
+        <Counter/>
+        {/* where everything re-renders when the count changes */}
+
+
+
+
+
         <RecoilRoot>
           <Counter2/>
         </RecoilRoot>
-        <Counter/>
+        
+
+
+        
+        <RecoilRoot>
+        
+        <Counter3/>
+        <Buttons/>
+        <IsEven/>
+        </RecoilRoot>
+
+
+
       </div>
   )
 }
+
+
+function Buttons(){
+  const setCount=useSetRecoilState(counterAtom);
+  return <div>
+    <button onClick={()=>setCount(count=>count+2)}>Increase</button>
+  <button onClick={()=>setCount(count=>count-1)}>Decrease</button>
+</div>
+}
+
+function Counter3(){
+  const count=useRecoilValue(counterAtom);
+  return <div>
+    {count}
+  </div>
+}
+
+
+// isEven is subscribed to the atom evenSelector so that it will re-render when the evenSelector changes that is the value of the count changes to odd
+function IsEven(){
+  const even=useRecoilValue(evenSelector);
+  return <div>
+    {even?"Even":"Odd"}
+  </div>
+}
+
+
+
+
+
+
+
+
 
 
 // recoil function only thr count will re-render and not the buttons 
@@ -26,7 +78,8 @@ function Counter2(){
   // if i add this line here const count=useRecoilValue(counterAtom); then
   // the entire component will re-render when the count changes
   // and all its children will re-render as well
-  // that's why we need to use memos or subsribe to the atom wherever we want to use it
+  // that's why we need to use memos with usestate or subsribe to the atom wherever we want to use it and that will re-render.
+  // Memos will prevent the component from re-rendering unless the props change
   return <div>
     <CurrentCount2/>
     <Increase2 />
@@ -41,12 +94,13 @@ function CurrentCount2(){
   </div>
 }
 
-function Increase2(){
+const Increase2=memo(function Increase2(){
   const setCount=useSetRecoilState(counterAtom);  
   return <div>
     <button onClick={()=>setCount(count=>count+1)}>Increase</button>
   </div>
-}
+});
+
 function Decrease2(){
   const setCount=useSetRecoilState(counterAtom);  
   return <div>
--- recoil/src/store/atoms/counter.js
+++ recoil/src/store/atoms/counter.js
@@ -1,4 +1,4 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
 
 
 // recoil
@@ -6,4 +6,12 @@ import { atom } from 'recoil';
 export const counterAtom=atom({
       key:"counter",
       default:0
+});
+
+export const evenSelector=selector({
+    key:"isEvenSelector",
+    get:function({get}){
+        const count=get(counterAtom);
+        return count%2==0;
+    }
 });
