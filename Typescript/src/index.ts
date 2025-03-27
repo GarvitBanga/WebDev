d65cb92e8df56737115acef05948eac5075fef8a
Garvit Banga <bangaboy181@gmail.com>
2025-03-27 02:04:49 -0400

    interfaces types abstract classes

--- Typescript/src/index.ts
+++ Typescript/src/index.ts
@@ -93,3 +93,258 @@ let a:string|number="hello";
 type stringornumber=string|number;
 let b:stringornumber="hello";
 b=10;
+
+
+// to make a field optional
+type optional={
+    name:string,
+    age?:number
+}
+
+let user5:optional={
+    name:"garvitbanga"
+}
+let user6:optional={
+    name:"garvitbanga",
+    age:26
+}
+// both are valid
+
+interface Useroptional{
+    name:string,
+    age:number,
+    address?:{
+        street:string,
+        city:string,
+        state:string,
+        zip:number
+    }
+};
+
+
+let user7:Useroptional={
+    name:"garvitbanga",
+    age:26,
+    address:{
+        street:"123 main street",
+        city:"hyderabad",
+        state:"telangana",
+        zip:400010
+    }
+}
+let user8:Useroptional={
+    name:"garvitbanga",
+    age:26
+}
+
+// this is not valid as either address you can have or not but if you have address then you must have all the fields
+// let user9:Useroptional={
+//     name:"garvitbanga",
+//     age:26,
+//     // address:{ zip missing
+//         street:"123 main street",
+//         city:"hyderabad",
+//         state:"telangana"
+//     }
+// }
+
+
+interface Address {
+    street:string,
+    city:string,
+    state:string,
+    zip:number
+};
+interface Userinterface{
+    name:string,
+    age:number,
+    address?:Address //you can have interface inside interface
+};
+
+
+interface People{
+    name:string,
+    age:number,
+    greet:()=>string //greet():string also does the same
+};
+
+
+let person1:People={
+    name:"garvitbanga",
+    age:26,
+    greet:()=>"hello"
+}
+console.log(person1.greet());
+// here the issue is inside the object greet function can't use name or age even though it's defined in the interface
+
+// So thats why we need classes that can implement interfaces
+class PeopleClass implements People{
+    name:string;
+    age:number;
+    constructor(name:string,age:number){
+        this.name=name;
+        this.age=age;
+    }
+    greet(){
+        return "hello1 "+this.name;
+    }
+}
+let person2=new PeopleClass("garvitbanga",26);
+console.log(person2.greet());
+
+
+// abstract class
+
+
+abstract class Peoplesabs{
+    name:string;
+    constructor(name:string){
+        this.name=name;
+    }
+    abstract greet():string;
+    hello(){
+        return "hi there";
+    }
+}
+
+class PeopleClassabs extends Peoplesabs{
+    name:string;
+    constructor(name:string){
+        super(name);
+        this.name=name;
+    }
+    greet(){
+        return "hello2 "+this.name;
+    }
+
+}
+let person3=new PeopleClassabs("garvitbanga");
+console.log(person3.greet());
+console.log(person3.hello());
+
+
+// TYPES
+// Intersections
+type Employee={
+    name:string,
+    startDate:Date
+}
+
+type Manager={
+    name:string,
+    department:string
+}
+
+type TeamLead= Employee & Manager;
+
+const teamlead1:TeamLead={
+    name:"garvitbanga",
+    startDate:new Date(),
+    department:"IT"
+}
+// need to have attributes from both types
+
+
+// UNIONS
+
+type newEmployee= Employee | Manager;
+
+// either Employee or Manager or both can be used
+const newEmployee1:newEmployee={
+    name:"garvitbanga",
+    startDate:new Date(),
+    department:"IT"
+}
+const newEmployee2:newEmployee={
+    name:"garvitbanga",
+    startDate:new Date(),
+    // department:"IT"
+}
+const newEmployee3:newEmployee={
+    name:"garvitbanga",
+    // startDate:new Date(),
+    department:"IT"
+}
+
+
+interface Admin{
+    name:string,
+    permission:string
+}
+interface User{
+    name:string,
+    age:number
+}
+
+type AdminUser=Admin|User;
+
+const adminuser1:AdminUser={
+    name:"garvitbanga",
+    permission:"admin"
+}
+function greetuseradmin(user:AdminUser){
+    return "helloadmin "+user.name;
+}
+console.log(greetuseradmin(adminuser1));
+
+
+// Arrays in typescript
+
+function getmax(nums:number[]):number{
+    let max=nums[0];
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]>max){
+            max=nums[i];
+        }
+    }
+    return max;
+
+}
+
+let arr:number[]=[1,2,3,4,5];
+console.log(getmax(arr));
+
+
+interface Userarr{
+    name:string,
+    age:number,
+    address:Address[]
+}
+// now here address can be an array of Address
+
+let user9:Userarr={
+    name:"garvitbanga",
+    age:26,
+    address:[
+        {
+            street:"123 main street",
+            city:"hyderabad",
+            state:"telangana",
+            zip:400010
+        },
+        {
+            street:"123 main street",
+            city:"hyderabad",
+            state:"telangana",
+            zip:400010
+        }
+    ]
+}
+
+console.log(user9.address);
+
+
+function filteruser(users:Userarr[]){
+    let ans=[];
+    for(let i=0;i<users.length;i++){
+        if(users[i].age>18){
+            ans.push(users[i]);
+        }
+    }
+    return ans;
+}
+let user10:Userarr[]=[
+    user9,
+    user9
+]
+console.log(filteruser(user10));
